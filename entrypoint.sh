a3b46e38b8c27d04229ef1b6ff7feb0e786a80ca
Hazegard <m.catrice@outlook.com>
2025-07-16 19:59:24 +0200

    star tworking on entrypoints

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+version="$(/app/server --version | awk -F'-' '{print $1}')"
+
+mkdir -p /app/binaries/old/"$version"
+
+cp -pu /app/build_binaries/* /app/binaries/old/"$version"
+cp -pu /app/build_binaries/* /app/binaries/
+
+/app/server -c /app/server_config.yaml -vvv
