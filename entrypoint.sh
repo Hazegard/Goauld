be4ef49486463fdd2ae898f2d54529b5d0cfbc9c
Hazegard <m.catrice@outlook.com>
2025-08-02 17:59:24 +0200

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 
 version="$(/app/server --version | awk -F'-' '{print $1}')"
 
-mkdir -p /app/binaries/old/"$version"
+mkdir -p /app/binaries/old/v"$version"
 
-cp -pu /app/build_binaries/* /app/binaries/old/"$version"
+cp -pu /app/build_binaries/* /app/binaries/old/v"$version"
 cp -pu /app/build_binaries/* /app/binaries/
 
 /app/server -c /app/server_config.yaml -vvv
