86131af311ffbbf0fa5170b7acd61b809a6c30b2
Hazegard <m.catrice@outlook.com>
2025-11-03 12:10:38 +0100

    garbelize the sshd windows shim

--- scripts/garble.sh
+++ scripts/garble.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [[ "$NO_LITERALS" == 1 ]]; then
-  garble -seed="$CLIENT__COMPILE_SEED" "$@"
+  garble -seed="$CLIENT__COMPILE_SEED" -tiny "$@"
 else
-  garble -literals -seed="$CLIENT__COMPILE_SEED" "$@"
+  garble -literals -seed="$CLIENT__COMPILE_SEED"  -tiny "$@"
 fi
