7351cd2a588907d874858f653e19e8532e22b761
Hazegard <m.catrice@outlook.com>
2026-03-14 15:54:11 +0100

    replace togettyc in repo

--- embed_client.go
+++ embed_client.go
@@ -13,7 +13,7 @@ import (
 // Sources embed the agent source code te be able to dynamically compile it
 //
 //nolint:revive
-//go:embed agent client common server vendored/github.com/hazegard/togettyc.tar.gz vendored/github.com/hazegard/socket.io-go.tar.gz vendored/github.com/aus/proxyplease@v0.1.0.tar.gz .goreleaser.yaml embed.go go.mod go.sum .env.build.tmpl scripts/garble.sh scripts/garble.bat scripts/shuffle/main.go
+//go:embed agent client common server vendored/github.com/hazegard/socket.io-go.tar.gz vendored/github.com/aus/proxyplease@v0.1.0.tar.gz .goreleaser.yaml embed.go go.mod go.sum .env.build.tmpl scripts/garble.sh scripts/garble.bat scripts/shuffle/main.go
 var Sources embed.FS
 
 // GetRoot returns the directory path of the current source file.
